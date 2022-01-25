35a23f8f3dbc9ef1786c7262d914bcba8190fcee
NevilleAdrian <neville.chukumah@gmail.com>
2022-01-25 22:49:47 +0100

    check survey function

--- Sources/BB/BB.swift
+++ Sources/BB/BB.swift
@@ -1,3 +1,4 @@
+import Foundation
 public struct BB {
     public private(set) var text = "Hello, World!"
 
@@ -6,5 +7,33 @@ public struct BB {
     
     public func HelloToYou() -> String{ 
         return "Hello to you"
+    }
+    
+    public func checkSurvey(token: String, id: String) {
+        guard let url = URL(string: "https://api.bitlabs.ai/v1/client/check?platform=WEB") else {
+            return
+        }
+        var request = URLRequest(url: url)
+        //method, headers
+        
+        request.httpMethod = "GET"
+        request.setValue("X-Api-Token", forHTTPHeaderField: token)
+        request.setValue("X-User-Id", forHTTPHeaderField: id)
+        
+        let task = URLSession.shared.dataTask(with: request){ data, _ , error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print("SUCCESS: \(response)")
+            }
+            catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
     }
 }
